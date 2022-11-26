87b523e282e6002f913cc35fd07c1d6db2c9a343
Youngwook Go <113055584+Youngwook-Go@users.noreply.github.com>
2022-11-26 04:01:10 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,33 @@
+// Created by: Youngwook
+// Created on: NOV 2022
+//
+// This function gets random number and user number
+// This function compares random number and user number and answer.
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // input
+        int age;
+        string day;
+
+        Console.WriteLine("This program gets user age and day of the week to answer properly.\n");
+
+        Console.Write("Enter your age : "); age = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter the day of the week : "); day = Console.ReadLine();
+
+        // output
+        if ((age > 12 && age < 21)||(day == "Tuesday" || day == "Thursday" ))
+        {
+            Console.WriteLine("You get in for free");
+        }
+        else
+        {
+            Console.WriteLine("You must pay admission");
+        }
+        Console.WriteLine("\ndone.");
+    }
+}
